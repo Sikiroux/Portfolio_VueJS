17dd3c9142555d3fa2c8f380181d109492c61f34
Sikiroux <damienekouma@gmail.com>
2023-06-15 15:52:45 -1000

    remove all vue basic

---
+++ Portfolio/src/components/Header.vue
@@ -0,0 +1,22 @@
+<script>
+
+</script>
+
+<template>
+    <header>
+        <div class="logo-holder"></div>
+        <ul>
+            <li>Présentation</li>
+            <li>Projet</li>
+            <li>Contact</li>
+        </ul>
+    </header>
+</template>
+
+
+<style>
+    header {
+        display: flex;
+        width: 100%;
+    }
+</style>
