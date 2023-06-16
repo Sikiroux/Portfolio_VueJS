a43776b7c7e9d944c2241222c5769ba33da0d96c
Sikiroux <damienekouma@gmail.com>
2023-06-15 16:31:54 -1000

    Create header component and add it to App.vue

--- Portfolio/src/App.vue
+++ Portfolio/src/App.vue
@@ -1,3 +1,9 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import TheHeader from './components/Header.vue'
 </script>
+
+<template>
+  <TheHeader></TheHeader>
+</template>
+
--- Portfolio/src/components/Header.vue
+++ Portfolio/src/components/Header.vue
@@ -5,11 +5,11 @@
 <template>
     <header>
         <div class="logo-holder"></div>
-        <ul>
-            <li>Présentation</li>
-            <li>Projet</li>
-            <li>Contact</li>
-        </ul>
+        <nav>
+            <a href="#">Présentation</a>
+            <a href="#">Créations</a>
+            <a href="#">Contact</a>
+        </nav>
     </header>
 </template>
 
@@ -18,5 +18,38 @@
     header {
         display: flex;
         width: 100%;
+        margin-top: 15px;
+        align-items: center;
+        border-bottom: 3px solid rgb(31, 30, 30);
+        padding-bottom: 20px;
     }
+    
+    .logo-holder {
+        height: 80px;
+        width: 80px;
+        background-image: url(./icons/Logo.png);
+        background-size: cover;
+        border-radius: 50%;
+        margin-left: 40px;
+    }
+    
+    nav a {
+        text-decoration: none;
+        color: white;
+        font-size: 1.5rem;
+    }
+
+    nav a:hover {
+        color: gray;
+    }
+
+    nav {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        text-align: center;
+        margin-right: 70px;
+        margin-left: 30%;
+        gap: 150px;
+    }
+
 </style>
